c03beedbf9caa11ee2fe127913308034f7492758
rhinoxD <coolshiv0721@gmail.com>
2021-11-25 11:37:19 +0530

    Adding Controllers

---
+++ controllers/places-controllers.js
@@ -0,0 +1,40 @@
+const HttpError = require('../models/http-error');
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl: 'https://static.toiimg.com/photo/71579199.cms',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    creator: 'u1',
+  },
+];
+
+exports.getPlaceById = (req, res, next) => {
+  const { pid } = req.params; // { pid: 'p1' }
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === pid;
+  });
+  if (!place) {
+    throw new HttpError('Could not find a place for the provided id.', 404);
+  }
+  res.json({ place }); // => { place } = { place: place }
+};
+
+exports.getPlaceByUserId = (req, res, next) => {
+  const { uid } = req.params;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === uid;
+  });
+  if (!place) {
+    return next(
+      new HttpError('Could not find a place for the provided user id.', 404)
+    );
+  }
+  res.json({ place });
+};
