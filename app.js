ac5925ef6cc74ac6e80b47a58d1084863f210272
rhinoxD <coolshiv0721@gmail.com>
2021-11-26 10:26:16 +0530

    Installing Mongoose and Connecting our Backend to MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -26,4 +27,13 @@ app.use((error, req, res, next) => {
     .json({ message: error.message || 'An unknown error occured!' });
 });
 
-app.listen(5000);
+mongoose
+  .connect(
+    'mongodb+srv://our-second-user:27MEmcrem0FeXs8i@cluster0.khfzh.mongodb.net/places?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
