16a627626f44bedfef2fe20717d892d4ab2f4b95
rhinoxD <coolshiv0721@gmail.com>
2021-11-27 10:06:54 +0530

    Initial Setup

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    'mongodb+srv://our-second-user:27MEmcrem0FeXs8i@cluster0.khfzh.mongodb.net/places?retryWrites=true&w=majority'
+    'mongodb+srv://our-second-user:27MEmcrem0FeXs8i@cluster0.khfzh.mongodb.net/mern?retryWrites=true&w=majority'
   )
   .then(() => {
     app.listen(5000);
