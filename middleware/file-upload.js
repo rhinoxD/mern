770e1ecf8f4767c2c47d53968ca2c71422fbc89f
rhinoxD <coolshiv0721@gmail.com>
2021-11-29 18:35:19 +0530

    On the Backend Using Multer to Save Files

---
+++ middleware/file-upload.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+const uuid = require('uuid').v4;
+
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpeg',
+};
+
+const fileUpload = multer({
+  limits: 10000000,
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/images');
+    },
+    filename: (req, file, cb) => {
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      cb(null, uuid() + '.' + ext);
+    },
+  }),
+});
+
+module.exports = fileUpload;
