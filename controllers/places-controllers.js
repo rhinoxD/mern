6972546353a94e4a96c1282e3768a44a2e4f2178
rhinoxD <coolshiv0721@gmail.com>
2021-11-29 19:23:16 +0530

    Deleting Images When Places Get Deleted

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
-const uuid = require('uuid').v4;
+const fs = require('fs');
+
 const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 
@@ -158,6 +159,7 @@ exports.deletePlace = async (req, res, next) => {
     const error = new HttpError('Could not find place for this id.', 404);
     return next(error);
   }
+  const imagePath = place.image;
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
@@ -172,5 +174,8 @@ exports.deletePlace = async (req, res, next) => {
     );
     return next(error);
   }
+  fs.unlink(imagePath, (err) => {
+    console.log(err);
+  });
   res.status(200).json({ message: 'Place Deleted.' });
 };
