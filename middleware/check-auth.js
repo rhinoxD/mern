fee47904ce41ed7b8053598e60c7e74bdd92fbfd
rhinoxD <coolshiv0721@gmail.com>
2021-11-30 15:32:46 +0530

    Using and Attaching JWT(Tokens) in React

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,8 +3,11 @@ const jwt = require('jsonwebtoken');
 const HttpError = require('../models/http-error');
 
 module.exports = (req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return next();
+  }
   try {
-    const token = req.headers.authentication.split(' ')[1];
+    const token = req.headers.authorization.split(' ')[1];
     if (!token) {
       throw new Error('Authentication failed!');
     }
