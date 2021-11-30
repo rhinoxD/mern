1a09c83a52cd6130c48eaa61f85c22417d01690a
rhinoxD <coolshiv0721@gmail.com>
2021-11-30 09:33:57 +0530

    Hashing the User Password

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const brcypt = require('bcryptjs');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -39,11 +40,22 @@ exports.signup = async (req, res, next) => {
     );
     return next(error);
   }
+  let hashedPassword;
+  try {
+    hashedPassword = await brcypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError(
+      'Could not create user, please try again.',
+      500
+    );
+    return next(error);
+  }
+
   const createdUser = new User({
     name,
     email,
     image: req.file.path,
-    password,
+    password: hashedPassword,
     places: [],
   });
   try {
