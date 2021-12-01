6e42663760c0f66b0f37acb625b255e38a5f6065
rhinoxD <coolshiv0721@gmail.com>
2021-12-01 19:08:21 +0530

    Added New React App Frontend Version

---
+++ public/precache-manifest.014a368d2aadefa2057168f2b34896f2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3447f9e9ffe6c45df1b9ae623a844e51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "260016aa9a68bb226850",
+    "url": "/static/css/0.ce71ab98.chunk.css"
+  },
+  {
+    "revision": "7f4aa013a21c1165bb20",
+    "url": "/static/css/4.2c55513f.chunk.css"
+  },
+  {
+    "revision": "a6723396147b27ec194b",
+    "url": "/static/css/5.4797a9a8.chunk.css"
+  },
+  {
+    "revision": "cd648cbecca47025b8ac",
+    "url": "/static/css/6.a60f70aa.chunk.css"
+  },
+  {
+    "revision": "48ed81c63fc83ebcba80",
+    "url": "/static/css/7.8e371d2c.chunk.css"
+  },
+  {
+    "revision": "066730ce379a99c6559f",
+    "url": "/static/css/8.7735a71b.chunk.css"
+  },
+  {
+    "revision": "7a5d4c497156f34c9860",
+    "url": "/static/css/main.72ec5e3d.chunk.css"
+  },
+  {
+    "revision": "260016aa9a68bb226850",
+    "url": "/static/js/0.37f6d942.chunk.js"
+  },
+  {
+    "revision": "a991d526841bd7eee98e",
+    "url": "/static/js/3.f89cc172.chunk.js"
+  },
+  {
+    "revision": "7f4aa013a21c1165bb20",
+    "url": "/static/js/4.86a2031e.chunk.js"
+  },
+  {
+    "revision": "a6723396147b27ec194b",
+    "url": "/static/js/5.c2a4d73f.chunk.js"
+  },
+  {
+    "revision": "cd648cbecca47025b8ac",
+    "url": "/static/js/6.a08ba8db.chunk.js"
+  },
+  {
+    "revision": "48ed81c63fc83ebcba80",
+    "url": "/static/js/7.eb6ff411.chunk.js"
+  },
+  {
+    "revision": "066730ce379a99c6559f",
+    "url": "/static/js/8.a32764ea.chunk.js"
+  },
+  {
+    "revision": "7a5d4c497156f34c9860",
+    "url": "/static/js/main.2fa443fd.chunk.js"
+  },
+  {
+    "revision": "45f3635543abaea0dafa",
+    "url": "/static/js/runtime-main.9671294f.js"
+  }
+]);
